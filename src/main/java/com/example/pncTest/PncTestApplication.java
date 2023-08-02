1de3aaef31c49e3d64c56ed6c071bb056e9f4a4e
Shardul Kavale <shardul.kavale@ibm.com>
2023-08-02 19:28:05 -0400

    Application Boilerplate Initialised

---
+++ src/main/java/com/example/pncTest/PncTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pncTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PncTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PncTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pncTest/PncTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pncTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PncTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
